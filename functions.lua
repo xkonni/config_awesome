cb4e7fa983a98567cc2869c6d86768e985e0b70b
xkonni <konstantin.koslowski@gmail.com>
2013-06-22 00:43:19 +0200

    awesome: update titlebar functions, clean up signals

--- functions.lua
+++ functions.lua
@@ -56,21 +56,8 @@ function formatstring(str, length)
 end
 
 function titlebar_enable(c)
-  -- Widgets that are aligned to the left
-  local left_layout = wibox.layout.fixed.horizontal()
-  left_layout:add(awful.titlebar.widget.iconwidget(c))
-
-  -- Widgets that are aligned to the right
-  local right_layout = wibox.layout.fixed.horizontal()
-  right_layout:add(awful.titlebar.widget.floatingbutton(c))
-  right_layout:add(awful.titlebar.widget.maximizedbutton(c))
-  right_layout:add(awful.titlebar.widget.stickybutton(c))
-  right_layout:add(awful.titlebar.widget.ontopbutton(c))
-  right_layout:add(awful.titlebar.widget.closebutton(c))
-
-  -- The title goes in the middle
-  local title = awful.titlebar.widget.titlewidget(c)
-  title:buttons(awful.util.table.join(
+  -- buttons for the titlebar
+  local buttons = awful.util.table.join(
           awful.button({ }, 1, function()
               client.focus = c
               c:raise()
@@ -81,13 +68,33 @@ function titlebar_enable(c)
               c:raise()
               awful.mouse.client.resize(c)
           end)
-          ))
+          )
+
+  -- Widgets that are aligned to the left
+  local left_layout = wibox.layout.fixed.horizontal()
+  left_layout:add(awful.titlebar.widget.iconwidget(c))
+  left_layout:buttons(buttons)
+
+  -- Widgets that are aligned to the right
+  local right_layout = wibox.layout.fixed.horizontal()
+  right_layout:add(awful.titlebar.widget.floatingbutton(c))
+  right_layout:add(awful.titlebar.widget.maximizedbutton(c))
+  right_layout:add(awful.titlebar.widget.stickybutton(c))
+  right_layout:add(awful.titlebar.widget.ontopbutton(c))
+  right_layout:add(awful.titlebar.widget.closebutton(c))
+
+  -- The title goes in the middle
+  local middle_layout = wibox.layout.flex.horizontal()
+  local title = awful.titlebar.widget.titlewidget(c)
+  title:set_align("center")
+  middle_layout:add(title)
+  middle_layout:buttons(buttons)
 
   -- Now bring it all together
   local layout = wibox.layout.align.horizontal()
   layout:set_left(left_layout)
   layout:set_right(right_layout)
-  layout:set_middle(title)
+  layout:set_middle(middle_layout)
 
   awful.titlebar(c):set_widget(layout)
 end
