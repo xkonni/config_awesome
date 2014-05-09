02128c06e9c13fcd3e6e13318ddaca98f0295154
konni <konstantin.koslowski@gmail.com>
2014-05-09 16:52:43 +0200

    awesome: remove scroll mousebindings from taglist

--- rc.lua
+++ rc.lua
@@ -72,9 +72,8 @@ mytaglist.buttons = awful.util.table.join(
   awful.button({ }, 1, awful.tag.viewonly),
   awful.button({ settings.modkey }, 1, awful.client.movetotag),
   awful.button({ }, 3, awful.tag.viewtoggle),
-  awful.button({ settings.modkey }, 3, awful.client.toggletag),
-  awful.button({ }, 4, function(t) awful.tag.viewnext(awful.tag.getscreen(t)) end),
-  awful.button({ }, 5, function(t) awful.tag.viewprev(awful.tag.getscreen(t)) end) )
+  awful.button({ settings.modkey }, 3, awful.client.toggletag)
+  )
 mytasklist = {}
 mytasklist.buttons = awful.util.table.join(
   awful.button({ }, 1, function (c)
