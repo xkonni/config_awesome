ac8d6294a09faffe7cb498df5c56acaa38657f3a
Konstantin Koslowski <konstantin.koslowski@gmail.com>
2014-08-10 09:54:02 +0200

    awesome: float youtube fullscreen windows

--- rc.lua
+++ rc.lua
@@ -439,7 +439,7 @@ awful.rules.rules = {
     properties = { floating = true } },
   { rule = { class = "pinentry" },
     properties = { floating = true } },
-  { rule = { class = "gimp" },
+  { rule = { class = "Plugin-container" },
     properties = { floating = true } },
   -- Set Firefox to always map on tags number 2 of screen 1.
   -- { rule = { class = "Firefox" },
